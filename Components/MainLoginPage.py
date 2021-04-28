7a86300109b8e72134da9b9f88e45ad25d3b7066
decadence127 <decadence127@gmail.com>
2021-04-29 01:09:28 +0300

    separated windows

--- Components/MainLoginPage.py
+++ Components/MainLoginPage.py
@@ -3,26 +3,30 @@ import psycopg2
 import tkinter.messagebox as box
 
 
+class Window(Toplevel):
+
+    def __init__(self, master=None):
+        super().__init__(master=master)
+
+
 def Database():
     global conn, cursor
     conn = psycopg2.connect(database="decadence", user="decadence",
                             password="12345", host="localhost", port="5432")
     cursor = conn.cursor()
+    print("Database opened successfully")
 
 
-print("Database opened successfully")
-
-
-root = Tk()
-root.title("Taxipark")
+master = Tk()
+master.title("Taxipark")
 width = 640
 height = 480
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+screen_width = master.winfo_screenwidth()
+screen_height = master.winfo_screenheight()
 x = (screen_width/2) - (width/2)
 y = (screen_height/2) - (height/2)
-root.geometry("%dx%d+%d+%d" % (width, height, x, y))
-root.resizable(0, 0)
+master.geometry("%dx%d+%d+%d" % (width, height, x, y))
+master.resizable(0, 0)
 
 
 # =======================================VARIABLES=====================================
@@ -31,13 +35,64 @@ PASS = StringVar()
 
 
 # =======================================METHODS=======================================
+def Register():
+    Database()
+    if USER.get == "" or PASS.get() == "":
+        lbl_result.config(
+            text="Please complete the required field!", fg="orange")
+    cursor.execute('SELECT * FROM public."user"')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+        if USER.get() == row[0]:
+            lbl_result.config(text="This user already exists", fg="orange")
+        else:
+            cursor.execute('INSERT INTO public."user" ("user", pass) VALUES (%s, %s)', (str(
+                USER.get()), str(PASS.get())))
+            conn.commit()
+            USER.set("")
+            PASS.set("")
+            print("Successfully Created!")
+            cursor.close()
+            conn.close()
+
+
+def RegistrationWindow(button):
+    registrationWindow = Window()
+    registrationWindow.title("Registration")
+    registrationWindow.geometry("640x480")
+    button.configure(state="disabled")
+
+    lbl_title.pack()
+    lbl_username = Label(registrationWindow, text="Username:",
+                         font=('arial', 18), bd=18)
+    lbl_username.grid(row=1)
+
+    lbl_password = Label(registrationWindow, text="Password:",
+                         font=('arial', 18), bd=18)
+    lbl_password.grid(row=2)
+
+    lbl_result = Label(registrationWindow, text="", font=('arial', 18))
+    lbl_result.grid(row=5, columnspan=2)
+
+    reg_user = Entry(registrationWindow, font=('arial', 20),
+                     textvariable=USER, width=15)
+    reg_user.grid(row=1, column=1)
+
+    pass2 = Entry(registrationWindow, font=('arial', 20),
+                  textvariable=PASS, width=15, show="*")
+    pass2.grid(row=2, column=1)
+
+    btn_register = Button(registrationWindow, font=('arial', 20),
+                          text="Register", command=Register)
+    btn_register.grid(row=6, columnspan=2)
 
 
 def Exit():
     result = tkMessageBox.askquestion(
         'System', 'Are you sure you want to exit?', icon="warning")
     if result == 'yes':
-        root.destroy()
+        master.destroy()
         exit()
 
 
@@ -57,59 +112,42 @@ def Login():
             lbl_result.config(text="Incorrect credentials!", fg="red")
 
 
-def Register():
-    Database()
-    if USER.get == "" or PASS.get() == "":
-        lbl_result.config(
-            text="Please complete the required field!", fg="orange")
-    else:
-        cursor.execute('SELECT * FROM public."user"')
-        cursor.execute('INSERT INTO public."user" ("user", pass) VALUES (%s, %s)', (str(
-            USER.get()), str(PASS.get())))
-        conn.commit()
-        USER.set("")
-        PASS.set("")
-        lbl_result.config(text="Successfully Created!", fg="green")
-        cursor.close()
-        conn.close()
-
-
 # =====================================FRAMES====================================
-TitleFrame = Frame(root, height=100, width=640, bd=1, relief=SOLID)
+TitleFrame = Frame(master, height=100, width=640, bd=1, relief=SOLID)
 TitleFrame.pack(side=TOP)
-RegisterFrame = Frame(root)
-RegisterFrame.pack(side=TOP, pady=20)
+LoginFrame = Frame(master)
+LoginFrame.pack(side=TOP, pady=20)
 
 
 lbl_title = Label(TitleFrame, text="Таксопарк",
                   font=('arial', 18), bd=1, width=640)
 lbl_title.pack()
-lbl_username = Label(RegisterFrame, text="Username:",
+lbl_username = Label(LoginFrame, text="Username:",
                      font=('arial', 18), bd=18)
 lbl_username.grid(row=1)
-lbl_password = Label(RegisterFrame, text="Password:",
+lbl_password = Label(LoginFrame, text="Password:",
                      font=('arial', 18), bd=18)
 lbl_password.grid(row=2)
-lbl_result = Label(RegisterFrame, text="", font=('arial', 18))
+lbl_result = Label(LoginFrame, text="", font=('arial', 18))
 lbl_result.grid(row=5, columnspan=2)
 
 
-user = Entry(RegisterFrame, font=('arial', 20),
+user = Entry(LoginFrame, font=('arial', 20),
              textvariable=USER, width=15)
 user.grid(row=1, column=1)
-pass1 = Entry(RegisterFrame, font=('arial', 20),
+pass1 = Entry(LoginFrame, font=('arial', 20),
               textvariable=PASS, width=15, show="*")
 pass1.grid(row=2, column=1)
 
-btn_register = Button(RegisterFrame, font=('arial', 20),
-                      text="Register", command=Register)
+btn_register = Button(LoginFrame, font=('arial', 20),
+                      text="Register", command=lambda: RegistrationWindow(btn_register))
 btn_register.grid(row=6, columnspan=2)
 
-btn_login = Button(RegisterFrame, font=('arial', 20),
+btn_login = Button(LoginFrame, font=('arial', 20),
                    text="Login", command=Login)
 btn_login.grid(row=6, columnspan=1)
 
 
 # ========================================INITIALIZATION===================================
 if __name__ == '__main__':
-    root.mainloop()
+    master.mainloop()
