ccaa1c8b4e46324557e863856b52eab4b4f7643a
decadence <decadence127@gmail.com>
2021-05-14 19:34:53 +0300

    Completed Login/Register system

--- Components/MainLoginPage.py
+++ Components/MainLoginPage.py
@@ -1,13 +1,14 @@
 
+from json import encoder
+import re
 import socket
 from tkinter import *
 import tkinter.messagebox as box
 import json
-from Components.classes import LoginModel
 
 
 HEADER = 64
-PORT = 5040
+PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -17,10 +18,22 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 
-class Window(Toplevel):
+class LoginModel:
+    login = ""
+    password = ""
+    login_token = 0
 
-    def __init__(self, master=None):
-        super().__init__(master=master)
+    def __init__(self) -> None:
+        pass
+
+    def __init__(self, login, password, login_token):
+        self.login = login
+        self.password = password
+        self.login_token = login_token
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
 
 
 master = Tk()
@@ -42,35 +55,43 @@ REGUSER = StringVar()
 REGPASS = StringVar()
 
 
-def RegistrationWindow(button):
-    registrationWindow = Window()
-    registrationWindow.title("Registration")
-    registrationWindow.geometry("640x480")
-    button.configure(state="disabled")
+def send_login_credentials(login, password, token):
+    if login.get() == "" or login.get() == "":
+        box.showerror("Error", "Enter User Name And Password")
+        return
+    else:
+        tempLogin = login.get()
+        tempPass = password.get()
+        loginData = LoginModel(tempLogin, tempPass, token)
+        JsonObject = loginData.toJSON()
+        serialized = json.dumps(JsonObject)
+        client.sendall(serialized.encode(FORMAT))
 
-    lbl_title.pack()
+
+def RegistrationWindow(button):
+    registrationWindow = Toplevel(master)
+    registrationWindow.title("Registration")
+    registrationWindow.geometry("480x280")
+    registrationWindow.resizable(False, False)
+
+    lbl_title.pack(side=TOP)
     lbl_username = Label(registrationWindow, text="Введите логин:",
-                         font=('arial', 18), bd=18)
+                         font=('roboto', 14), bd=14)
     lbl_username.grid(row=1)
 
     lbl_password = Label(registrationWindow, text="Введите пароль:",
-                         font=('arial', 18), bd=18)
+                         font=('roboto', 14), bd=14)
     lbl_password.grid(row=2)
 
-    lbl_result = Label(registrationWindow, text="", font=('arial', 18))
-    lbl_result.grid(row=5, columnspan=2)
-
-    reg_user = Entry(registrationWindow, font=('arial', 20),
+    reg_user = Entry(registrationWindow, font=('roboto', 16),
                      textvariable=REGUSER, width=15)
     reg_user.grid(row=1, column=1)
 
-    pass2 = Entry(registrationWindow, font=('arial', 20),
-                  textvariable=REGPASS, width=15, show="*")
-    pass2.grid(row=2, column=1)
-    registrationData = LoginModel(REGUSER.get(), REGPASS.get(), 1)
-
-    btn_register = Button(registrationWindow, font=('arial', 20),
-                          text="Register", command=send_login_credentials(registrationData))
+    reg_pass = Entry(registrationWindow, font=('roboto', 16),
+                     textvariable=REGPASS, width=15, show="*")
+    reg_pass.grid(row=2, column=1)
+    btn_register = Button(registrationWindow, font=('roboto', 16),
+                          text="Зарегистрировать", command=lambda: send_login_credentials(REGUSER, REGPASS, 1))
     btn_register.grid(row=6, columnspan=2)
 
 
@@ -83,11 +104,6 @@ def send(msg):
     client.send(message)
 
 
-def send_login_credentials(registrationData):
-    pickleregistrationData = json.dumps(registrationData.toJSON())
-    client.sendall(pickleregistrationData)
-
-
 TitleFrame = Frame(master, height=100, width=640, bd=1, relief=SOLID)
 TitleFrame.pack(side=TOP)
 LoginFrame = Frame(master)
@@ -95,31 +111,30 @@ LoginFrame.pack(side=TOP, pady=20)
 
 
 lbl_title = Label(TitleFrame, text="Таксопарк",
-                  font=('arial', 18), bd=1, width=640)
+                  font=('roboto', 18), bd=1, width=640)
 lbl_title.pack()
 lbl_username = Label(LoginFrame, text="Введите логин:",
-                     font=('arial', 18), bd=18)
+                     font=('roboto', 14), bd=18)
 lbl_username.grid(row=1)
 lbl_password = Label(LoginFrame, text="Введите пароль:",
-                     font=('arial', 18), bd=18)
+                     font=('roboto', 14), bd=18)
 lbl_password.grid(row=2)
 lbl_result = Label(LoginFrame, text="", font=('arial', 18))
 lbl_result.grid(row=5, columnspan=2)
 
 
-user = Entry(LoginFrame, font=('arial', 20),
+user = Entry(LoginFrame, font=('roboto', 16),
              textvariable=LOGINUSER, width=15)
 user.grid(row=1, column=1)
-pass1 = Entry(LoginFrame, font=('arial', 20),
+pass1 = Entry(LoginFrame, font=('roboto', 16),
               textvariable=LOGINPASS, width=15, show="*")
 pass1.grid(row=2, column=1)
-loginData = LoginModel(LOGINUSER.get(), LOGINPASS.get(), 0)
-btn_register = Button(LoginFrame, font=('arial', 20),
-                      text="Register", command=lambda: RegistrationWindow(btn_register))
-btn_register.grid(row=6, columnspan=2)
+btn_register = Button(LoginFrame, font=('roboto', 16),
+                      text="Зарегистрироваться", command=lambda: RegistrationWindow(btn_register))
+btn_register.grid(row=6, columnspan=3, padx=180)
 
-btn_login = Button(LoginFrame, font=('arial', 20),
-                   text="Login", command=lambda: send_login_credentials(loginData))
+btn_login = Button(LoginFrame, font=('roboto', 16),
+                   text="Войти", command=lambda: send_login_credentials(LOGINUSER, LOGINPASS, 0))
 btn_login.grid(row=6, columnspan=1)
 
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import threading
 
 HEADER = 64
 FORMAT = 'utf-8'
-PORT = 5040
+PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -19,8 +19,8 @@ except socket.error as e:
 def Database():
 
     global conn, cursor
-    conn = psycopg2.connect(database="decadence", user="decadence",
-                            password="12345", host="localhost", port="5432")
+    conn = psycopg2.connect(database="decadence", user="postgres",
+                            password="12344321", host="localhost", port="5432")
     cursor = conn.cursor()
     print("Database opened successfully")
 
@@ -39,25 +39,24 @@ def handle_client(conn, addr):
 
     connected = True
     while connected:
-        recvallObject = bytearray()
-        while True:
-            recvObject = conn.recv(4096)
-            if not recvObject:
-                break
-            print('Recv: {}: {}'.format(len(recvObject), recvObject))
-            recvallObject += recvObject
-            break
-        obj = json.loads(recvallObject)
-        print("obj:", obj)
+        received = conn.recv(1024)
+        received = received.decode(FORMAT)
+        try:
+            deserialized = json.loads(received)
+        except (TypeError, ValueError) as e:
+            raise Exception('Data received was not in JSON format')
+        print("obj:", deserialized)
+        res = json.loads(deserialized)
 
-        # if
-        # msg_name_length = conn.recv(HEADER).decode(FORMAT)
-        # if msg_name_length:
-        #     msg_name_length = int(msg_name_length)
-        #     msg_name = conn.recv(msg_name_length).decode(FORMAT)
+        if(int(res["login_token"]) == 0):
+            Login(res["login"], res["password"])
 
-        #     if msg_name == DISCONNECT_MESSAGE:
-        # Login(msg_name, msg_password)
+        elif(int(res["login_token"]) == 1):
+            Register(res["login"], res["password"])
+
+        else:
+            print("Error")
+
     conn.close()
 
 
@@ -97,21 +96,16 @@ def Login(msg_name, msg_password):
         server.send(message)
 
 
-# def Register(msg_login, msg_password):
-#     Database()
-#     if  == "" or REGPASS.get() == "":
-#         lbl_result.config(
-#             text="Please complete the required field!", fg="orange")
-#     cursor.execute('SELECT * FROM public."user"')
+def Register(msg_login, msg_password):
+    Database()
+    cursor.execute('SELECT * FROM public."user"')
 
-#     cursor.execute('INSERT INTO public."user" ("user", pass) VALUES (%s, %s)', (str(
-#         REGUSER.get()), str(REGPASS.get())))
-#     conn.commit()
-#     REGUSER.set("")
-#     REGPASS.set("")
-#     print("Successfully Created!")
-#     cursor.close()
-#     conn.close()
+    cursor.execute('INSERT INTO public."user" ("user", pass) VALUES (%s, %s)',
+                   (msg_login, str(msg_password)))
+    conn.commit()
+    print("Successfully Created!")
+    cursor.close()
+    conn.close()
 
 
 def start():
